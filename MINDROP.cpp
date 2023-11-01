5b5b80c00b383ed29f69ccc22f5042c759da7bad
sterben-209 <khangphanhuynh2007@gmail.com>
2023-11-01 20:40:59 +0700

    1

--- MINDROP.cpp
+++ MINDROP.cpp
@@ -1,11 +1,31 @@
 // https://ntucoder.net/Problem/Details/4489?
 
 #include<iostream>
+#include<algorithm>
+#include<map>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
-    
+    long n,b; cin >>n; long m = 0; 
+    long tmp,temp,a=1;
+    for (long i = 0; i<n;i++ ) {
+        if(i > 0){
+            cin >> tmp;
+            if(tmp == temp) a++;
+            else a = 1;
+            temp = tmp;
+        }else{
+            cin>>tmp;   
+            temp = tmp; 
+        }
+        m = std::max(m,a);
+
+    }
+
+    cout <<  n - m ; 
+
 
 
     return 0;
